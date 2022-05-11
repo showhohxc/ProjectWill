60ccf8109758da2dc31da311516457f803d4f96e
showhohxc <showhohxc@gmail.com>
2022-05-11 18:13:53 +0900

    언리얼 자습 20220511 18:13

--- FirstProject/Source/FirstProject/Floater.cpp
+++ FirstProject/Source/FirstProject/Floater.cpp
@@ -8,7 +8,7 @@
 AFloater::AFloater()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	StaticMeshPerty = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CustomStaticMesh"));
 
